f9e9f5401c126cdeb152ac2c9f604fa30b867004
tehforsch <mail@tonipeter.de>
2022-06-04 14:48:02 +0200

    disable clippy warnings for functions with many args / long types (bevys queries)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+// Some or our '*_system' functions have a large number of arguments.
+// That is not necessarily a bad thing, as they are auto-provided by bevy.
+#![allow(clippy::too_many_arguments)]
+// Some of the Query<…> types appear rather complex to clippy, but are actually
+// perfectly readable.
+#![allow(clippy::type_complexity)]
+
 mod config;
 mod create_grid;
 mod mpi_world;
